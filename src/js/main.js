423c1d00bb16bbe25db52cae8456baad628e8a6e
Mr_Omid <terminator444411@gmail.com>
2023-02-05 15:44:27 +0330

    header search updated

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,24 @@ $(document).ready(function(){
 
 
 });
+
+
+// search box
+var search_items_list = document.getElementById('search_items_list');
+const search_input = document.getElementById('search_input');
+var searched_items = document.getElementById('searched_items');
+search_items_list.classList.add('d-none');
+
+search_input.addEventListener('focus',function(){
+
+        search_items_list.classList.remove('d-none');
+});
+
+search_input.addEventListener('blur',function(){
+search_items_list.classList.add('d-none');
+
+});
+
+search_input.addEventListener('keyup',function(){
+    searched_items.innerText = search_input.value;
+});
